e21c44d3161a3c3c36daf8bf074564d80230652a
jikongabc <3387649844@qq.com>
2026-04-26 21:17:46 +0800

    create game page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">My game</h1>'
+    line2 = '<hr>'
+    line3 = '<a href="/play/">进入游戏界面</a>'
+    return HttpResponse(line1 + line3 + line2)
+
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line2 = '<hr>'
+    line3 = '<a href="/">回到主界面</a>'
+    return HttpResponse(line1 + line3 + line2)
